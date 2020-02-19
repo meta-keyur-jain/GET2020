17cbcace6ed0063e455db3c719a5d72e9e247b59
meta-keyur-jain <59638397+meta-keyur-jain@users.noreply.github.com>
2020-02-19 17:48:06 +0530

    Add files via upload

---
+++ EADAssignment7a/src/main/java/com/metacube/assignment/EadAssignment7aApplication.java
@@ -0,0 +1,13 @@
+package com.metacube.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadAssignment7aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadAssignment7aApplication.class, args);
+	}
+
+}
