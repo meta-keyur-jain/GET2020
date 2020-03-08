6d3e6140ea6b04094f6b0ce5d6faae0ba6a4e33b
meta-keyur-jain <59638397+meta-keyur-jain@users.noreply.github.com>
2020-03-08 20:17:29 +0530

    Add files via upload

---
+++ MetaParkingSystemV6/src/main/java/com/metacube/metaparkingsystemv2/dao/EmployeeDao.java
@@ -0,0 +1,22 @@
+package com.metacube.metaparkingsystemv2.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+//import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.metacube.metaparkingsystemv2.models.Employee;
+
+public interface EmployeeDao extends CrudRepository<Employee, Integer> {
+
+	@Query("Select empId from Employee where emailId = :emailId")
+	int findEmpIdByEmailId(@Param("emailId") String emailId);
+	
+	@Query("from Employee where orgName = :orgName and empId <> :empId")
+	List<Employee> findFriends(@Param("orgName") String orgName, @Param("empId") int empId);
+		
+}
