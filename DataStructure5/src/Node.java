4e9c57529792d705a82adad4096aa0072ec93472
meta-keyur-jain <59638397+meta-keyur-jain@users.noreply.github.com>
2020-04-06 11:53:53 +0530

    Add files via upload

---
+++ DataStructure5/src/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+
+    int key;
+    String value;
+    Node leftChild, rightChild;
+
+   
+    public Node(int key, String value) {
+        this.key = key;
+        this.value = value;
+        leftChild = null;
+        rightChild = null;
+    }
+}
